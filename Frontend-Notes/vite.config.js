7b52396ba8b3f8a0984a04d92bdb0bfb0f9584dc
Roshan-Here <leoparmigiana@gmail.com>
2024-02-13 00:28:16 +0530

    frontend setup complete

--- Frontend-Notes/vite.config.js
+++ Frontend-Notes/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '192.168.1.7',
+    port: 8111,
+  }
 })
